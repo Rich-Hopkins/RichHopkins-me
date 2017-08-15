e4e77922234677f91485e3a78c768a8b20ac2a59
Rich Hopkins <hopmedic@hotmail.com>
2017-08-15 19:47:40 -0400

    Slideshow concept working. Will work on custom photos.

--- RH-WebApi/app/views/home/homeController.js
+++ RH-WebApi/app/views/home/homeController.js
@@ -7,7 +7,25 @@
 
 	function Home() {
 		var vm = this;
-		vm.eaa = "EAA 690";
+		vm.slides = [];
+		vm.interval = 5000;
+		vm.noWrapSlides = false;
+		vm.active = 0;
+		vm.currIndex = 0;
+		
+		vm.slides.push({
+			image: 'app/images/Tri-Motor-3x2-600w.jpg',
+			title: 'NC8407',
+			desc: '1929 Ford Tri-Motor in flight',
+			id: vm.currIndex++
+		},
+			{
+				image: 'app/images/Tri-Motor-Tail-9x16-270w.jpg',
+				title: 'NC8407',
+				desc: 'Tail of 1929 Ford Tri-Motor',
+				id: vm.currIndex++
+			});
+
 		return vm;
 	};
 
