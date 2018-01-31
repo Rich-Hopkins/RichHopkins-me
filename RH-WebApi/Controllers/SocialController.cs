91e2bfce44adf54a2add95fb99ef949d5ca8ea32
Richard Hopkins <hopmedic@hotmail.com>
2018-01-31 11:16:31 -0500

    Added conditional to Social api and cleaned it up.

--- RH-WebApi/Controllers/SocialController.cs
+++ RH-WebApi/Controllers/SocialController.cs
@@ -11,24 +11,34 @@ namespace RH_WebApi.Controllers
   public class SocialController : ApiController
   {
     [HttpGet]
-    public HttpResponseMessage Get()//(string page)
+    public HttpResponseMessage Get(string page)
     {
-      var html = "<!DOCTYPE html>\n<html>\n<head lang=\"en\">\n";
-      html += "<meta charset=\"UTF-8\">\n";
-      html += "<meta http-equiv=\"X-UA-Compatible\"content=\"IE=edge\">\n";
-      html += "<meta property=\"og:type\" content=\"website\">\n";
-      html += "<meta property=\"og:url\" content=\"https://richhopkins.me/cbindex\">\n";
-      html += "<meta property=\"og:title\" content=\"Carrie Brown Keto Recipe Index\">\n";
-      html+= "<meta property=\"og:description\" content =\"Carrie Brown is a world class chef who creates great ketogenic recipes. This is an easy-to-use index of all of her publicly-available recipes.\">\n";
-      html += "<meta property=\"og:image\" content=\"http://carriebrown.com/wp-content/uploads/2017/10/Carrie-1-1.jpg\"/>\n";
-      html += "</head>\n<body>\n";
-      html += "<p>Carrie Brown Keto Recipe Index</p>";
-      html+= "<img src=\"http://carriebrown.com/wp-content/uploads/2017/10/Carrie-1-1.jpg\"/>\n";
-      html += "</body>\n</html>"; 
+      var html = "";
+      if (page.ToLower() == "cbindex")
+      {
+        html = BuildHtml();
+        html += "<meta property=\"og:url\" content=\"https://richhopkins.me/cbindex\">";
+        html += "<meta property=\"og:title\" content=\"Carrie Brown Keto Recipe Index\">";
+        html += "<meta property=\"og:description\" content =\"Carrie Brown is a world class chef who creates great ketogenic recipes. This is an easy-to-use index of all of her publicly-available recipes.\">";
+        html += "<meta property=\"og:image\" content=\"http://carriebrown.com/wp-content/uploads/2017/10/Carrie-1-1.jpg\"/>";
+        html += "</head><body>";
+        html += "<p>Carrie Brown Keto Recipe Index</p>";
+        html += "<img src=\"http://carriebrown.com/wp-content/uploads/2017/10/Carrie-1-1.jpg\"/>";
+        html += "</body></html>";
+      }
 
       var response = new HttpResponseMessage();
       response.Content = new StringContent(html, Encoding.UTF8, "text/html");
       return response;
+    }
+
+    private static string BuildHtml()
+    {
+      var html = "<!DOCTYPE html><html><head lang=\"en\">";
+      html += "<meta charset=\"UTF-8\">";
+      html += "<meta http-equiv=\"X-UA-Compatible\"content=\"IE=edge\">";
+      html += "<meta property=\"og:type\" content=\"website\">";
+      return html;
     }
   }
 }
