98a033ea509626f39a1242d8e24638e33bcdd791
Richard Hopkins <hopmedic@hotmail.com>
2018-02-01 22:40:46 -0500

    Updating look of cbindex view

---
+++ RH-WebApi/app/scripts/Directives/rhHeader.js
@@ -0,0 +1,12 @@
+﻿(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .directive('rhHeader',
+      function () {
+        return {
+          templateUrl: 'app/views/templates/header.html'
+        };
+      });
+})();
